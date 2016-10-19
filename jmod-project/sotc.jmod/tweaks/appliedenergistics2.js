059a6561aa36f8e8daeae32eb6051a3b2b6d82fb
Nicholas Marshall <nmarshall23@gmail.com>
2016-10-19 18:59:35 -0400

    Ideas for AE Tweaks

--- jmod-project/sotc.jmod/tweaks/appliedenergistics2.js
+++ jmod-project/sotc.jmod/tweaks/appliedenergistics2.js
@@ -1,3 +1,119 @@
+load("functions.js");
+
+log('Loaded Applied Energistics2 Tweaks');
+
+/*
+
+var dustRedstone = "dustRedstone";
+var dustGlowstone;
+var ingotAluminum = "ingotAluminum";
+var ingotConstantan = "ingotConstantan";
+var ingotSteel = "ingotSteel";
+
+var plateAluminum = "plateAluminum";
+var plateConstantan = "plateConstantan";
+var plateSteel = "plateSteel";
+
+
+
+var blockGlass;
+
+var gemDiamond = "gemDiamond";
+
+var glassQuartz;
+var crystalFluix;
+
+var me1kStorageComponent;
+var me4kStorageComponent;
+var me16kStorageComponent;
+var me64kStorageComponent;
+
+var me1kStorageCell;
+var me4kStorageComponent;
+var me16kStorageComponent;
+var me64kStorageComponent;
+
+var coreAnnihilation;
+var coreFormation;
+
+var processorCalculation;
+var processorEngineering;
+
+var meGlassCable;
+var meTerminal;
+
+var meStorageHousing = {
+ name: "",
+ recipes: [
+  [glassQuartz, dustRedstone, glassQuartz],
+  [dustRedstone, null, dustRedstone],
+  [plateAluminum, plateAluminum, plateAluminum]
+ ]
+};
+
+updateShappedRecipe(meStorageHousing);
+
+var meAdvancedCard = {
+ name: "",
+ num: 2,
+ recipes: [
+  [gemDiamond, plateAluminum, null],
+  [dustRedstone, processorCalculation, ingotAluminum],
+  [gemDiamond, plateAluminum, null]
+ ]
+};
+
+var meBasicCard = {
+ name: "",
+ num: 2,
+ recipes: [
+  [gemDiamond, plateAluminum, null],
+  [dustRedstone, processorCalculation, ingotAluminum],
+  [gemDiamond, plateAluminum, null]
+ ]
+};
+
+var meDrive = {
+ name: "",
+ recipes: [
+  [plateSteel, processorEngineering, plateSteel],
+  [meGlassCable, null, meGlassCable],
+  [plateSteel, processorEngineering, plateSteel]
+ ]
+};
+
+var meChest = {
+ name: "",
+ recipes: [
+  [blockGlass, meTerminal, blockGlass],
+  [meGlassCable, null, meGlassCable],
+  [plateSteel, blockGlass, plateSteel]
+ ]
+};
+
+var planelIlluminated = {
+ name: "",
+ num: 2,
+ recipes: [
+  [null,            dustGlowstone, glassQuartz],
+  [ingotConstantan, dustRedstone,  glassQuartz],
+  [null,            dustGlowstone, glassQuartz]
+ ]
+};
+
+var meInterface = {
+ name: "",
+ num: 2,
+ recipes: [
+  [plateSteel,       blockGlass, plateSteel],
+  [coreAnnihilation, null,  coreFormation],
+  [plateSteel,       blockGlass, plateSteel]
+ ]
+};
+
+*/
+
+
 // Charger
 
 
